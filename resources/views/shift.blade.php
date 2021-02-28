be6a1ac44ffc2ede679110f9c4a98122c753fc44
adhiiimf <yoimapren27@gmail.com>
2021-02-28 10:31:55 +0700

    Add Shift features

--- resources/views/shift.blade.php
+++ resources/views/shift.blade.php
@@ -25,10 +25,10 @@
     <div class="modal-dialog modal-dialog-centered">
       <div class="modal-background">
         <div class="p-4 text-center">
-            <span class="text-center rec-title">Buat Akun CaAs</span>
+            <span class="text-center rec-title">Buat Shifts Baru</span>
         </div>
         <div class="modal-body  text-center">
-            <form method="POST" action="\AddCaas">
+        <form method="POST" action="\addShift">
                 @csrf
             <div class="pt-2 pb-2">
                 <input class="text-center text-area-fill" type="date" name="hari" placeholder="tanggal" required>
@@ -42,7 +42,7 @@
         </div>
         <div class="modal-footer d-flex justify-content-center">
             <button type="button" class="button-cancel" data-bs-dismiss="modal">Batal</button>
-            <button type="submit" class="button-submit">Buat Akun</button>
+            <button type="submit" class="button-submit">Simpan</button>
           </div>
         </form>
       </div>
@@ -71,9 +71,7 @@
           </div>
           <div class="text-center pt-3 pb-3">
             <div>
-                <form action="/CariNIM" method="GET">
-                <input autocomplete="off" class="form-style-find" type="text" id="find" name="find" alt="find" placeholder="Cari NIM..."><button type="submit" class="button-submit-find ms-1">CARI</button>
-                </form>
+                <a href=""><button type="submit" class="button-submit-find ms-1">Lihat Plottingan</button></a>
               </div>
           </div>
         </div>
@@ -82,10 +80,10 @@
         <div class="card-body">
             <div class="d-flex justify-content-center pb-4">
                 <div>
-                    <button type="button" class="button-submit-find" data-bs-toggle="modal" data-bs-target="#CaasInput">Input Akun CaAs</button>
+                    <button type="button" class="button-submit-find" data-bs-toggle="modal" data-bs-target="#CaasInput">Buat Shift Baru</button>
                 </div>
                 <div class="ms-2">
-                <a href="/CaasAccount"><button type="button" class="button-submit-find">
+                <a href="/ListShift"><button type="button" class="button-submit-find">
                 Refresh 
                 </button></a>
                 </div>
@@ -96,33 +94,24 @@
             <table class="table table-bordered table-hover table-striped text-center align-middle">
                 <thead>
                     <tr>
-                        <th>Hari</th>
+                        <th>Tanggal</th>
                         <th>Jam</th>
                         <th>Kuota</th>
-                        <th>List CaAs</th>
                         <th>OPSI</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {{-- @foreach($caas as $p)
+                    @foreach($shift as $p)
                     <tr>
-                        <td>{{ $p->nama }}</td>
-                        <td style="color:black;font-weight:700;background-color:#f1b442">{{ $p->nim }}</td>
-                        <td>{{ $p->email }}</td>
-                        @if($p->isLolos==1)
-                        <td style="color:rgb(20, 182, 20);font-weight:700">Lolos</td>
-                        @else
-                        <td style="color:red;font-weight:700">Tidak Lolos</td>
-                        @endif
+                        <td>{{ $p->hari }}</td>
+                        <td>{{ $p->jam }}</td>
+                        <td>{{ $p->kuota }}</td>
                         <td>
-                        
-                        </td>
-                        <td>
-                            <a href="/EditCaasAccount/{{ $p->datacaas_id }}"><button style="font-size: 1rem" class="button-submit-find">Edit</button></a>
-                            <a href="/delcaasconfirm/{{ $p->datacaas_id }}" ><button class="button-cancel">Hapus</button></a>             
+                            <a href="/EditShift/"><button style="font-size: 1rem" class="button-submit-find">Edit</button></a>
+                            <a href="/delShiftconfirm/" ><button class="button-cancel">Hapus</button></a>             
                         </td>
                     </tr>
-                    @endforeach --}}
+                    @endforeach
                 </tbody>
             </table>
         </div>
