d80e4027d0fb02a32a3e0d77164da6cd58fd531b
adhiiimf <yoimapren27@gmail.com>
2021-03-13 23:26:30 +0700

    Add Modify navbar blade

--- resources/views/CaasAccount.blade.php
+++ resources/views/CaasAccount.blade.php
@@ -68,20 +68,11 @@
       </div>
     </div>
   </div>
-<section id="nav-section">
-    <nav class="navbar navbar-expand-lg dlor-navbar">
-        <div class="container-fluid">
-          <a class="navbar-brand" href=""><img src="{{asset('/assets/dlor.png')}}" alt="logo" class="dlor-logonav"></a>
-          <div class="dlor-navright" id="dlor-toggler">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link text-center" href="/adminHome" tabindex="-1" aria-disabled="true"><img src="{{ asset('/assets/back-icon-admin.png') }}" alt="icon" width="40px" height="40px"></a>
-                </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-</section> 
+
+@include('layouts/navbarAdminBack')
+@section('navbackadmin')
+@endsection
+
 <section id="list-section">
 <div class="container pb-5">
     <div class="pt-5 d-flex justify-content-center">
