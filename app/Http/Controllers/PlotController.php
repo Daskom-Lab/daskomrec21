b8cd3d0abf7e378d16dc3c46489c60496c133539
adhiiimf <yoimapren27@gmail.com>
2021-03-21 07:54:16 +0700

    Add Logistik model and Carbon Local indonesia

--- app/Http/Controllers/PlotController.php
+++ app/Http/Controllers/PlotController.php
@@ -13,6 +13,7 @@ use App\Models\Shift;
 use App\Models\Plot;
 use App\Models\Plotactive;
 use App\Models\Firstmeet;
+use App\Models\Logistik;
 use App\Models\Messageceklulus;
 use Route;
 use App\Http\Controllers\Controller;
@@ -25,24 +26,28 @@ class PlotController extends Controller
         $namatahap = Namatahap::all();
         $ceklulus = Ceklulus::where('id',1)->first();
         $caas = Datacaas::all();
+        \Carbon\Carbon::setLocale('id');
         $plot = PLot::leftjoin('shifts','shifts.id','=','plots.shifts_id')
             ->leftjoin('datacaas','datacaas.id','=','plots.datacaas_id')->get();
         return view('resultplot',compact('namatahap','ceklulus','countshift','shift','caas','plot'));
     }
     public function logistikview(){
+        $id = Auth::id();
+        $logistik = Logistik::find($id);
         $shift = Shift::orderBy('hari','asc')->orderBy('jam_start','asc')->paginate(10);
         $countshift = Shift::count();
         $namatahap = Namatahap::all();
         $ceklulus = Ceklulus::where('id',1)->first();
         $caas = Datacaas::all();
+        \Carbon\Carbon::setLocale('id');
         $plot = PLot::leftjoin('shifts','shifts.id','=','plots.shifts_id')
             ->leftjoin('datacaas','datacaas.id','=','plots.datacaas_id')->get();
-        return view('logistikplot',compact('namatahap','ceklulus','countshift','shift','caas','plot'));
+        return view('logistikplot',compact('namatahap','ceklulus','countshift','shift','caas','plot','logistik'));
     }
 
     public function listplot() {
         $id = Auth::id();
-        $shift = Shift::paginate(10);
+        $shift = Shift::orderBy('hari','asc')->orderBy('jam_start','asc')->paginate(10);
         $caas = Datacaas::where('datacaas.id',$id)
                     ->leftjoin('statuses','datacaas.id','=','statuses.datacaas_id')
                     ->leftjoin('tahaps','tahaps.id','=','statuses.tahaps_id')
@@ -62,6 +67,7 @@ class PlotController extends Controller
                     ->leftjoin('namatahaps','statustahaps.current_tahap','=','namatahaps.id')->first();
         $ceklulus = Ceklulus::where('id',1)->first();
         $firstmeet = Firstmeet::find(1);
+        \Carbon\Carbon::setLocale('id');
         if($caas->isLolos==1 && $statustahap->current_tahap==$caas->urut_tahap && $ceklulus->isPlotRun==1 && $plotactive->isPlotActive==NULL){
             return view('plotchoose',compact('shift','caas','plotactive','sisakuota','plots','statustahap')); 
         }
@@ -94,6 +100,7 @@ class PlotController extends Controller
         $limit = $shift->kuota - $sisakuota;
         $ceklulus = Ceklulus::where('id',1)->first();
         $firstmeet = Firstmeet::find(1);
+        \Carbon\Carbon::setLocale('id');
         if($caas->isLolos==1 && $plotactive->isPlotActive==NULL && $ceklulus->isPlotRun==1 && $statustahap->current_tahap==$caas->urut_tahap){
             return view('takeplot',compact('shift','caas','plotactive','limit','ceklulus','firstmeet')); 
         }elseif($caas->isLolos==0 && $statustahap->current_tahap==$caas->urut_tahap && $firstmeet->isPlotFirstmeet==1 && $plotactive->isPlotActive==NULL && $caas->urut_tahap==1 ){
@@ -166,6 +173,7 @@ class PlotController extends Controller
                     ->leftjoin('namatahaps','statustahaps.current_tahap','=','namatahaps.id')->first();
         $ceklulus = Ceklulus::where('id',1)->first();
         $firstmeet = Firstmeet::find(1);
+        \Carbon\Carbon::setLocale('id');
         if($caas->isLolos==1 && $statustahap->current_tahap==$caas->urut_tahap && $plotactive->isPlotActive==1){
             return view('finalPlot',compact('caas','plotactive','plots','statustahap','firstmeet')); 
         }elseif($caas->isLolos==0 && $caas->urut_tahap==1 && $plotactive->isPlotActive==1){
