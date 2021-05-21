ef8686f552ea3ab5c559a7868d9a306870c71c6f
Adhi MF <66113867+adhiiimf@users.noreply.github.com>
2021-05-21 08:34:46 +0700

    Renew text for footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -3,7 +3,7 @@
     <footer class="container">
     <div class="d-flex justify-content-center">
       <div>
-        <span style="font-size: 13px">Created By Chef of Daskomlab</span>
+        <span style="font-size: 13px">Official Daskom Lab Recruitment 2021</span>
       </div>
     </div>
     </footer>
